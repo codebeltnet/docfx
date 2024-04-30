aecf1397501996c78a7e18cb0ddd5dcf108f825c
Michael Mortensen <michael@geekle.io>
2024-04-30 12:59:28 +0200

    chore: include latest tag to complement actual bump in version

--- BuildAndPushDocfxImage.ps1
+++ BuildAndPushDocfxImage.ps1
@@ -12,6 +12,8 @@
     [Parameter(ValueFromPipeline)]
     [string]$DockerFile="Dockerfile"
 )
-Invoke-Expression "docker buildx build --build-arg=DOCFX_VERSION=$($DocfxVersion) --build-arg=DOTNET_VERSION=$($DotnetVersion) -t $($Tag):$($DocfxVersion) --platform $($Platform) --load -f Dockerfile ."
+Invoke-Expression "docker buildx build --build-arg=DOCFX_VERSION=$($DocfxVersion) --build-arg=DOTNET_VERSION=$($DotnetVersion) -t $($Tag):$($DocfxVersion) -t $($Tag):latest --platform $($Platform) --load -f Dockerfile ."
 Invoke-Expression "docker tag $($Tag):$($DocfxVersion) $($ContainerRegistry)$($Tag):$($DocfxVersion)"
+Invoke-Expression "docker tag $($Tag):$($DocfxVersion) $($ContainerRegistry)$($Tag):latest"
 Invoke-Expression "docker push $($ContainerRegistry)$($Tag):$($DocfxVersion)"
+Invoke-Expression "docker push $($ContainerRegistry)$($Tag):latest"
--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,13 @@
-﻿FROM mcr.microsoft.com/dotnet/sdk:8.0-jammy
+﻿FROM mcr.microsoft.com/dotnet/sdk:8.0-alpine
 ARG DOCFX_VERSION=2.75.3
 ARG DOTNET_VERSION=8.0
 
 ENV DOTNET_ROOT=/root/.dotnet
 ENV PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
 
-RUN apt update \
-    && apt install git
+RUN apk update --no-cache \
+    && apk add --no-cache git \
+    && apk add --no-cache bash
 
 RUN git clone https://github.com/dotnet/docfx.git \
     && mkdir -p /opt/docfx
@@ -14,6 +15,8 @@ RUN git clone https://github.com/dotnet/docfx.git \
 RUN cd docfx \
     && cp -r --parents templates/default /opt/docfx \
     && cp -r --parents templates/modern /opt/docfx
+
+RUN rm -r docfx
 
 RUN curl -sSL --output dotnet-install.sh https://dot.net/v1/dotnet-install.sh \
     && chmod +x ./dotnet-install.sh \
